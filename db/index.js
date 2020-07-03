c48d9a1a64c17ed0cea60a5d623838d20e588b3d
Joshua Farayola <farayolajoshua@gmail.com>
2020-07-03 01:26:29 +0100

    updated db config for test

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ import config from '../config';
 
 const connect = async () => {
   try {
-    await mongoose.connect(config.mongoTestUrl, {
+    await mongoose.connect(config.mongoUrl, {
       useUnifiedTopology: true,
       useNewUrlParser: true
     });
--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
+
+  preset: '@shelf/jest-mongodb',
+
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -129,7 +132,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  // testEnvironment: "node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
